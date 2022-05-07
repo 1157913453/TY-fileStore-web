0e66fde0b7de0f86c20b34588136c4b9f7d8a095
root <root@zr.localdomain>
2022-05-07 10:31:09 +0800

    11

--- src/request/user.js
+++ src/request/user.js
@@ -22,6 +22,6 @@ import { get, post } from './http'
 // 用户登录
 export const login = (p) => get('/user/login', p)
 // 获取登录状态及用户信息
-export const checkUserLoginInfo = (p) => get('/user/checkuserlogininfo', p)
+export const checkUserLoginInfo = (p) => get('/user/checkUserLoginInfo', p)
 // 用户注册
 export const addUser = (p) => post('/user/register', p)
